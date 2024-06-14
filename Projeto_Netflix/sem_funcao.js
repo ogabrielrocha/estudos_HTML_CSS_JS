0fea077acd7c36494009e76edb954b47783b985d
Gabriel Rocha <84602458+ogabrielrocha@users.noreply.github.com>
2024-06-14 15:00:00 -0300

    Create sem_funcao.js

---
+++ Projeto_Netflix/sem_funcao.js
@@ -0,0 +1,8 @@
+var listaDeFilmes = ["https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZGE1MDg5M2MtNTkyZS00MTY5LTg1YzUtZTlhZmM1Y2EwNmFmXkEyXkFqcGdeQXVyNjA3OTI0MDc@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMTU1MTkxNzc5NF5BMl5BanBnXkFtZTgwOTM2Mzk3MTI@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+
+
+for (i = 0; i < listaDeFilmes.length; i++){
+  document.write("<img src=" + listaDeFilmes[i] + ">" )
+  
+  
+}// Fim do For
